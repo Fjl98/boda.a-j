9bd86b11df3d2302ee54173371256081b6628888
fjl98 <jesuss.laguna@alumnos.cei.es>
2023-03-08 20:22:21 +0100

    2ver

--- js/funciones.js
+++ js/funciones.js
@@ -11,3 +11,38 @@ hamburgerMenu.addEventListener('click', () => {
     menuAbierto = true;
   }
 });
+
+
+
+// Establecer la fecha del evento
+var countDownDate = new Date("July 22, 2023 00:00:00").getTime();
+
+// Actualizar el contador cada segundo
+var x = setInterval(function() {
+
+  // Obtener la fecha actual
+  var now = new Date().getTime();
+
+  // Calcular la diferencia entre la fecha actual y la fecha del evento
+  var distance = countDownDate - now;
+
+  // Calcular los meses, días, horas, minutos y segundos restantes
+  var months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30));
+  var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Mostrar los valores en los elementos correspondientes
+  document.getElementById("months").innerHTML = months;
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // Si la cuenta atrás termina, mostrar un mensaje
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "¡Ya ha llegado!";
+  }
+}, 1000);
