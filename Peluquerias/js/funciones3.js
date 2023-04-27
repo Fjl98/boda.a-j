428470f2516bdd165bc7632409e57bd5461c1ec2
Fjl98 <116802483+Fjl98@users.noreply.github.com>
2023-04-27 11:27:38 +0200

    v3

---
+++ Peluquerias/js/funciones3.js
@@ -0,0 +1,16 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menuDesplegable = document.querySelector('.menu-desplegable');
+let menuAbierto = false;
+
+hamburgerMenu.addEventListener('click', () => {
+  if (menuAbierto) {
+    menuDesplegable.classList.remove('activo');
+    menuAbierto = false;
+  } else {
+    menuDesplegable.classList.add('activo');
+    menuAbierto = true;
+  }
+});
+
+
+
